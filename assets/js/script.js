a83e85d0c9d8683b0807671ebc389679f8f42685
unknown <blchilds55@gmail.com>
2020-11-06 18:05:03 -0600

    added a function to grab movie suggestions based on the user's search and pass the suggestions to the getInfo function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//api key: b7c4be357f010f43e3a269dfe7a54a8d
+
 var infoEl = document.querySelector("#info-container");
 var trailerEl = document.querySelector("#trailer-container");
 var searchBtnEl = document.querySelector("#search-button");
@@ -5,8 +7,6 @@ var formEl = document.querySelector("#form-container");
 var watchEl = document.querySelector("#watch-later-btn");
 
 var watchHandler = function(event){
-
-    console.log("hi");
 
 }
 
@@ -20,19 +20,58 @@ var searchHandler = function(event){
 }
 
 var getTrailer = function(data){
-    console.log(data);
 
 }
 
 var getInfo = function(data){
 
-console.log(data);
-
-
 
 }
 
+var getSuggestions = function() {
+    event.preventDefault();
+
+    var searchGrab = document.querySelector("#search-input").value;
+    var apiUrl = "https://api.themoviedb.org/3/search/movie?api_key=b7c4be357f010f43e3a269dfe7a54a8d&query=" + searchGrab;
+ 
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json().then(function(response) {
+                var movieArray = response.results;
+                displaySuggestions(movieArray, searchGrab);
+        })
+    })
+}
+
+var displaySuggestions = function(movieArray, searchGrab) {
+    var movieID = "";
+    
+    for(i = 0; i < movieArray.length; i++) {
+        if (movieArray[i].title === searchGrab) {
+            movieID = movieArray[i].id;
+            break;
+        }
+    }
+    
+    var apiUrl = "https://api.themoviedb.org/3/movie/" + movieID + "/recommendations?api_key=b7c4be357f010f43e3a269dfe7a54a8d&language=en-US"
+    
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json().then(function(response) {
+                var suggestionsArray = [];
+
+                for (i = 0; i < 5; i++) {
+                    suggestionsArray.push(response.results[i].title);
+                }
+                console.log(suggestionsArray);
+                getInfo(suggestionsArray);
+            })
+        })
+}
+
+
 
 formEl.addEventListener("submit", searchHandler);
+formEl.addEventListener("submit", getSuggestions);
 watchEl.addEventListener("click", watchHandler);
 
